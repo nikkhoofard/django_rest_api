535a4458727ab9d1052a6673ec09c380c53182dd
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-03-05 21:44:02 +0330

    CREATE APP AND MODEL AND SERIALIZER AND URLS FILE

--- django-rest-api/api/models.py
+++ django-rest-api/api/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import CASCADE
+
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=160)
+    link = models.URLField()
+    teacher = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.title
